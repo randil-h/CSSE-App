23c38e36a158eb25e1f3422b86e16e7371734850
Scheffel333 <118644309+Scheffel333@users.noreply.github.com>
2024-10-13 23:19:49 +0530

    customers

--- frontend/src/pages/customers_pages/Registration.js
+++ frontend/src/pages/customers_pages/Registration.js
@@ -29,7 +29,7 @@ export default function Registration() {
     const customerData = { ...formData };
 
     try {
-      const response = await axios.post('https://csse-backend.vercel.app/registration', customerData);
+      const response = await axios.post('https://csse-backend.vercel.app/customers', customerData);
 
       if (response.status === 201) { // Assuming 201 means successful registration
         console.log('Customer registered successfully');
